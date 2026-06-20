ea268d4d9d41bb63ba6f0bd9bb80dc796d2b3723
KJESS1 <jessicauduud@gmail.com>
2026-06-20 11:28:35 +0000

    Fix ESM import for @mysten/sui/grpc in sui.cjs

--- server/sui.cjs
+++ server/sui.cjs
@@ -1,11 +1,21 @@
-const { Transaction } = require('@mysten/sui/transactions');
-const { SuiGrpcClient } = require('@mysten/sui/grpc');
-const { Ed25519Keypair } = require('@mysten/sui/keypairs/ed25519');
+let clientPromise = null;
 
-const client = new SuiGrpcClient({ network: 'testnet', baseUrl: 'https://fullnode.testnet.sui.io:443' });
-const adminKeypair = Ed25519Keypair.fromSecretKey(process.env.ADMIN_PRIVATE_KEY);
+async function getClient() {
+  if (clientPromise) return clientPromise;
+  clientPromise = (async () => {
+    const { Transaction } = await import('@mysten/sui/transactions');
+    const { SuiGrpcClient } = await import('@mysten/sui/grpc');
+    const { Ed25519Keypair } = await import('@mysten/sui/keypairs/ed25519');
+
+    const client = new SuiGrpcClient({ network: 'testnet', baseUrl: 'https://fullnode.testnet.sui.io:443' });
+    const adminKeypair = Ed25519Keypair.fromSecretKey(process.env.ADMIN_PRIVATE_KEY);
+    return { Transaction, client, adminKeypair };
+  })();
+  return clientPromise;
+}
 
 async function resolveGame(gameId, winnerAddress) {
+  const { Transaction, client, adminKeypair } = await getClient();
   const tx = new Transaction();
   tx.moveCall({
     target: `${process.env.PACKAGE_ID}::game_escrow::resolve_game`,
@@ -15,6 +25,7 @@ async function resolveGame(gameId, winnerAddress) {
 }
 
 async function houseJoinGame(gameId, stakeMist) {
+  const { Transaction, client, adminKeypair } = await getClient();
   const tx = new Transaction();
   const [coin] = tx.splitCoins(tx.gas, [stakeMist]);
   tx.moveCall({
